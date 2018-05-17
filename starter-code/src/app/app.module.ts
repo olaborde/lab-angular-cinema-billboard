566e96dcb08b4dc48938ae479861a1c8a64e7b4d
olaborde <oml007@live.com>
2018-05-17 10:55:02 -0400

    done

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -2,19 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MovieService } from './services/movie.service';
+import { MovieComponent } from './movie/movie.component';
+
+
+const routes:Routes =[
+{path: '', component: HomeComponent},
+{path: 'movies/:id', component: MovieComponent}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [MovieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
